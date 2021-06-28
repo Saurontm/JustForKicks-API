f585bc5b16b938f8c4bec0a3632e183b81a67046
Sarah Almahmid <hpsarontm@gmail.com>
2021-06-28 20:03:28 +0300

    added products

---
+++ products.js
@@ -0,0 +1,33 @@
+const products = [
+  {
+    id: 1,
+    slug: "nike-air-force-1-low-psychic-blue",
+    name: "Nike Air Force 1 Low Psychic Blue",
+    price: 60,
+    imageURL:
+      "https://sneakernews.com/wp-content/uploads/2021/02/Nike-Air-Force-1-CZ0337-400-Release-Info-8.jpg",
+    description:
+      "Forward, much of the design is quite ordinary, leveraging a white leather construction like most GRs. The mid-panel, then, subtly introduces the Neapolitan-esque gradient, with greys marking atop in a more tumbled finish. Adjacent, the lace unit, Swoosh, and the majority of the back heel display the titular “Psychic Blue,” complimenting both the woven label as well as the lining and insole. What’s more, the colorway also features some subtle patterns: the dubrae lightly airbrushes with green steaks, while the heel tab does the same though with a darker blue.",
+  },
+  {
+    id: 2,
+    name: "Nike Air Force 1 Low Purple Suit",
+    slug: "nike-air-force-1-low-purple-suit",
+    price: 70,
+    imageURL:
+      "https://sneakernews.com/wp-content/uploads/2021/06/nike-air-force-1-low-purple-DN5063-500-5.jpg?w=1140",
+    description:
+      "Despite the Easter holiday being several weeks behind us and Spring making way for the hotter summer months, Nike is pressing the gas on pastel-hued kicks. We’ve seen that seasonal aesthetic in recent Air Force 1s as well as Nike Dunks, and it arrives once again the a premium Air Force 1 Low that tackles the color purple in a soft, muted approach. The upper features contrasting materials of fine grain suedes, while the leather deploys a slightly darker shade. Add in a solid white outsole and you have a sneaker that perfectly achieves that clean two-toned appeal that has been the flawless formula of AF1s.",
+  },
+  {
+    id: 3,
+    name: "The Nike Air Force 1 Low Happy Pineapple",
+    slug: "nike-air-force-1-low-happy-pineapple",
+    price: 65,
+    imageURL:
+      "https://sneakernews.com/wp-content/uploads/2021/06/Nike-Air-Force-1-Low-CZ0268-300-8.jpg?w=1140",
+    description:
+      "Previously seen in a predominantly-cork arrangement, the newly-surfaced proposition indulges in a summer-ready ensemble clad in shades of “Green Glow” and “Coconut Milk.” Canvas material across the shoe’s upper delivers a lighter stride than traditional leather would, and when paired with the tropical hues throughout the tongue, profile swooshes and spine, it becomes appropriate of the upcoming season.",
+  },
+];
+module.exports = products;
