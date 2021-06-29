f8ee6eed7c4b78de49cefec5bbacafb581ca1c00
Sarah Almahmid <hpsarontm@gmail.com>
2021-06-29 18:24:29 +0300

    Delete Product

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const products = require("./products");
+var products = require("./products");
 const cors = require("cors");
 
 const app = express();
@@ -8,6 +8,17 @@ app.use(cors());
 
 app.get("/products", (req, res) => {
   res.json(products);
+});
+
+app.delete("/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const foundProduct = products.find((sneaker) => sneaker.id === +productID);
+  if (foundProduct) {
+    products = products.filter((sneaker) => sneaker.id !== +productID);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: `Sneaker not found of ${productID}` });
+  }
 });
 
 app.listen(8000, () => {
