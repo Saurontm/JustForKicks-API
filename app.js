2c2a783c75cbeda7a44b59cdcfd403b3542e955c
Sarah Almahmid <hpsarontm@gmail.com>
2021-07-04 19:01:28 +0300

    connect app to database

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const sneakerRoutes = require("./API/sneaker/routes");
 const app = express();
+const db = require("./db/models");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/products", sneakerRoutes);
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
-});
+const run = async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log("Connection to database successful!");
+    await app.listen(8000, () => {
+      console.log("The application is running on localhost:8000");
+    });
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+};
+
+run();
