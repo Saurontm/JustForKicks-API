47adbf85a88beb00d6a272594f201c676f5aefec
Sarah Almahmid <hpsarontm@gmail.com>
2021-07-05 19:21:54 +0300

    updated controllers

--- API/sneaker/controllers.js
+++ API/sneaker/controllers.js
@@ -6,41 +6,43 @@ exports.sneakerFetch = async (req, res) => {
     const products = await Product.findAll({
       attributes: { exclude: ["createdAt", "updatedAt"] },
     });
-    console.log(products);
     res.json(products);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-exports.deleteSneaker = (req, res) => {
-  const { productID } = req.params;
-  const foundProduct = products.find((sneaker) => sneaker.id === +productID);
-  if (foundProduct) {
-    products = products.filter((sneaker) => sneaker.id !== +productID);
-    res.status(204).end();
-  } else {
-    res.status(404).json({ message: `Sneaker not found of ${productID}` });
+exports.createSneaker = async (req, res) => {
+  try {
+    const newSneaker = await Product.create(req.body);
+    res.status(201).json(newSneaker);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
-exports.createSneaker = (req, res) => {
-  const id = products.length + 1;
-  const slug = slugify(req.body.name, { lower: true });
-  const newSneaker = { id: id, slug: slug, ...req.body };
-  products.push(newSneaker);
-  res.status(201).json(newSneaker);
+exports.deleteSneaker = async (req, res) => {
+  const { productID } = req.params;
+  try {
+    const foundSneaker = await Product.findByPk(productID);
+    if (foundSneaker) {
+      await foundSneaker.destroy();
+      res.status(204).end();
+    }
+  } catch (error) {
+    res.status(404).json({ message: "product not found" });
+  }
 };
 
-exports.updateSneaker = (req, res) => {
+exports.updateSneaker = async (req, res) => {
   const { productID } = req.params;
-  const foundProduct = products.find((sneaker) => sneaker.id === +productID);
-  if (foundProduct) {
-    for (const key in req.body) foundProduct[key] = req.body[key];
-    foundProduct.slug = slugify(foundProduct.name, { lower: true });
-    foundProduct.price = +foundProduct.price;
-    res.status(204).end();
-  } else {
-    res.status(404).json({ message: `Sneaker not found of ${productID}` });
+  try {
+    const foundSneaker = await Product.findByPk(productID);
+    if (foundSneaker) {
+      await foundSneaker.update(req.body);
+      res.status(204).end();
+    }
+  } catch (error) {
+    res.status(404).json({ message: "product not found" });
   }
 };
--- db/models/Product.js
+++ db/models/Product.js
@@ -1,8 +1,13 @@
+const SequelizeSlugify = require("sequelize-slugify");
+
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("Product", {
+  const Product = sequelize.define("Product", {
     name: { type: DataTypes.STRING },
+    slug: { type: DataTypes.STRING, unique: true },
     price: { type: DataTypes.INTEGER },
     imageURL: { type: DataTypes.STRING },
     description: { type: DataTypes.STRING },
   });
+  SequelizeSlugify.slugifyModel(Product, { source: ["name"] });
+  return Product;
 };
